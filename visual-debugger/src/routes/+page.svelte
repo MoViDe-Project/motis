b6fb562b247d340f307b2afa2367d6f457d3f115
Dominik Kuckeburg <n.kuckeburg@outlook.de>
2024-12-18 19:18:49 +0100

    style: improve layout of +page.svelte

--- visual-debugger/src/routes/+page.svelte
+++ visual-debugger/src/routes/+page.svelte
@@ -4,9 +4,31 @@
     import QueryBatchOverview from '$lib/components/ui/QueryBatchOverview.svelte';
 </script>
 
-<img src="/logo.svg" alt="MoViDe logo" style="width: 100px; height: 100px;">
-<h1>Welcome to MoViDe (MOTIS Visual Debugger)</h1>
+<style>
+    .center {
+        display: block;
+        margin-left: auto;
+        margin-right: auto;
+    }
+    .horizontal-container {
+        display: flex;
+        align-items: center;
+        gap: 1rem; /* space between elements */
+    }
+</style>
 
-<FileUpload />
-<Button title="Upload bestätigen"/>
-<QueryBatchOverview />
+<img src="/logo.svg" alt="MoViDe logo" style="width: 100px; height: 100px;" class="center">
+
+<div class="horizontal-container">
+    <h1>Welcome to MoViDe (MOTIS Visual Debugger)</h1>
+</div>
+
+<div class="horizontal-container">    
+    <FileUpload />
+    <Button title="Confirm Upload"/>
+</div>
+
+<div class="horizontal-container">
+    <QueryBatchOverview />
+    <!-- TODO journey Details to the right of the query batch overview -->
+</div>
