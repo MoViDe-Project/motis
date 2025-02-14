4dcd1a9e9aef7b1391b5fbe593b932ee6d0d0849
Toni Nguyen <d3vzero@outlook.de>
2025-02-14 19:17:53 +0100

    feat: Add tests to changePlan

--- visual-debugger/src/data-processing/tests/changeElements.test.ts
+++ visual-debugger/src/data-processing/tests/changeElements.test.ts
@@ -1,9 +1,38 @@
 import { changePlan } from "@data/changeElements"
+import { Plan } from "@data/type-declarations/planTypes"
+import { activeQueryStore, currentDefaultPlanStore, currentPlanStore, defaultPlanDatasetStore, planDatasetStore } from "sveltestore"
 import { test, expect, vi } from "vitest"
 
-vi.mock("sveltestore");
+// TODO: Mock stores properly(?)
+// vi.mock('sveltestore')
+
+// Static inputs
+const inputPlan: Plan = new Plan()
+const defaultPlan: Plan = new Plan()
 
 test('changePlan: Happy path', () => {
+    // Init inputs
+    const index: number = 1
+    planDatasetStore.set([inputPlan]);
+    defaultPlanDatasetStore.set([defaultPlan])
+
+    // Init changed variables
+    let activeQuery: number = -1 
+    let currentPlan: Plan | undefined = undefined
+    let currentDefaultPlan: Plan | undefined = undefined
+
+    // Access stores
+    activeQueryStore.subscribe((d) => {activeQuery = d})
+    currentPlanStore.subscribe((d) => {currentPlan = d})
+    currentDefaultPlanStore.subscribe((d) => {currentDefaultPlan = d})
+
+    // Function call
+    changePlan(index)
+
+    // Assertions
+    expect(activeQuery).toBe(1)
+    expect(currentPlan).toBe(inputPlan)
+    expect(currentDefaultPlan).toBe(defaultPlan)
 })
 
 test('changePlan: Index out of bounds', () => {
@@ -11,9 +40,17 @@ test('changePlan: Index out of bounds', () => {
 })
 
 test('changePlan: planDatesetStore is empty', () => {
+    planDatasetStore.set([]);
+    defaultPlanDatasetStore.set([defaultPlan])
+
+    expect(changePlan(1)).toThrowError()
 })
 
 test('changePlan: defaultPlanDatasetStore is empty', () => {
+    planDatasetStore.set([inputPlan]);
+    defaultPlanDatasetStore.set([])
+
+    expect(changePlan(1)).toThrowError()
 })
 
 test('changeItinerary: Happy path', () => {
