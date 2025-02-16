33ffd88b97562d5263e9265be4b77e43159b609a
Toni Nguyen <d3vzero@outlook.de>
2025-02-16 16:19:03 +0100

    feat: Add tests for comparePlans

--- visual-debugger/src/data-processing/tests/comparePlans.test.ts
+++ visual-debugger/src/data-processing/tests/comparePlans.test.ts
@@ -1,12 +1,53 @@
 import { vi, test, expect } from "vitest"
 import { comparePlans } from '@data/comparePlans'
+import { Itinerary, Plan } from "@data/type-declarations/planTypes"
+import { currentDefaultPlanStore, currentPlanStore, defaultPlanDatasetStore, planDatasetStore } from "sveltestore"
+import { cssClasses } from "@data/styling/cssClasses"
 
-vi.mock('sveltestore')
+// Static inputs
+const inputPlan: Plan = new Plan()
+const defaultPlan: Plan = new Plan()
+const inputItinerary: Itinerary = new Itinerary()
+const defaultItinerary: Itinerary = new Itinerary()
 
-test('comparePlans: Happy path')
+inputPlan.itineraries = [ inputItinerary ] 
+defaultPlan.itineraries = [ defaultItinerary ] 
 
-test('comparePlans: planDatasetStore is empty')
+test('comparePlans: Happy path', () => {
+    planDatasetStore.set([inputPlan])
+    defaultPlanDatasetStore.set([defaultPlan])
 
-test('comparePlans: defaultPlanDatesetStore is empty')
+    let currentPlan: Plan = new Plan()
+    let currentDefaultPlan: Plan = new Plan()
 
-test('comparePlans: plans and defaultPlans mismatch in length')
+    currentPlanStore.subscribe((d) => { currentPlan = d })
+    currentDefaultPlanStore.subscribe((d) => { currentDefaultPlan = d })
+
+    comparePlans()
+
+    // Test store contents
+    expect(currentPlan).toBe(inputPlan)
+    expect(defaultPlan).toBe(defaultPlan)
+
+    // Test CSS classes
+    expect(currentPlan.itineraries[0].cssClass).toBe(cssClasses.planEntryValid)
+    expect(currentDefaultPlan.itineraries[0].cssClass).toBe(cssClasses.planEntryValid)
+})
+
+test('comparePlans: planDatasetStore is empty', () => {
+    // TODO: Include proper spy for function alert
+
+    planDatasetStore.set([])
+    defaultPlanDatasetStore.set([defaultPlan])
+    
+    expect(comparePlans()).toThrowError()
+})
+
+test('comparePlans: defaultPlanDatesetStore is empty', () => {
+    // TODO: Include proper spy for function alert
+
+    planDatasetStore.set([inputPlan])
+    defaultPlanDatasetStore.set([])
+
+    expect(comparePlans()).toThrowError()
+})
