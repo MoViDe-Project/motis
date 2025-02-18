5c008fceecbd78e4483720e3b918d0f28c7e22c8
nicosvv <nicolas.schmidt@live.de>
2025-02-18 12:38:24 +0100

    feat: add dynamic items to all leg attributes

--- visual-debugger/src/lib/components/ui/subcomponents/LegEntry.svelte
+++ visual-debugger/src/lib/components/ui/subcomponents/LegEntry.svelte
@@ -38,27 +38,97 @@
             </span>
         </div>
         <div>
-            <span>Start time: {leg.startTime}</span>
+            <span>
+                {#if shadowLeg.startTime}
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+                     stroke="#26a269" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                     class="lucide lucide-check"><path d="M20 6 9 17l-5-5"/></svg>
+                {:else}
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+                     stroke="#a51d2d" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                     class="lucide lucide-x"><path d="M18 6 6 18"/><path d="m6 6 12 12"/></svg>
+                {/if}
+                Start time: {leg.startTime}</span>
         </div>
         <div>
-            <span>End time: {leg.endTime}</span>
+            <span>
+                {#if shadowLeg.endTime}
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+                     stroke="#26a269" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                     class="lucide lucide-check"><path d="M20 6 9 17l-5-5"/></svg>
+                {:else}
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+                     stroke="#a51d2d" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                     class="lucide lucide-x"><path d="M18 6 6 18"/><path d="m6 6 12 12"/></svg>
+                {/if}
+                End time: {leg.endTime}</span>
         </div>
         <div>
-            <span>Scheduled start time: {leg.scheduledStartTime}</span>
+            <span>
+                {#if shadowLeg.scheduledStartTime}
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+                     stroke="#26a269" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                     class="lucide lucide-check"><path d="M20 6 9 17l-5-5"/></svg>
+                {:else}
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+                     stroke="#a51d2d" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                     class="lucide lucide-x"><path d="M18 6 6 18"/><path d="m6 6 12 12"/></svg>
+                {/if}
+                Scheduled start time: {leg.scheduledStartTime}</span>
         </div>
     </div>
     <div class="leading-9">
         <div>
-            <span>Scheduled end time: {leg.scheduledEndTime}</span>
+            <span>
+                {#if shadowLeg.scheduledEndTime}
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+                     stroke="#26a269" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                     class="lucide lucide-check"><path d="M20 6 9 17l-5-5"/></svg>
+                {:else}
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+                     stroke="#a51d2d" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                     class="lucide lucide-x"><path d="M18 6 6 18"/><path d="m6 6 12 12"/></svg>
+                {/if}
+                Scheduled end time: {leg.scheduledEndTime}</span>
         </div>
         <div>
-            <span>Realtime: {leg.realTime}</span>
+            <span>
+                {#if shadowLeg.realTime}
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+                     stroke="#26a269" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                     class="lucide lucide-check"><path d="M20 6 9 17l-5-5"/></svg>
+                {:else}
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+                     stroke="#a51d2d" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                     class="lucide lucide-x"><path d="M18 6 6 18"/><path d="m6 6 12 12"/></svg>
+                {/if}
+                Realtime: {leg.realTime}</span>
         </div>
         <div>
-            <span>Headsign: {leg.headsign}</span>
+            <span>
+                {#if shadowLeg.headsign}
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+                     stroke="#26a269" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                     class="lucide lucide-check"><path d="M20 6 9 17l-5-5"/></svg>
+                {:else}
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+                     stroke="#a51d2d" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                     class="lucide lucide-x"><path d="M18 6 6 18"/><path d="m6 6 12 12"/></svg>
+                {/if}
+                Headsign: {leg.headsign}</span>
         </div>
         <div>
-            <span>Route short name: {leg.routeShortName}</span>
+            <span>
+                {#if shadowLeg.routeShortName}
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+                     stroke="#26a269" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                     class="lucide lucide-check"><path d="M20 6 9 17l-5-5"/></svg>
+                {:else}
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+                     stroke="#a51d2d" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                     class="lucide lucide-x"><path d="M18 6 6 18"/><path d="m6 6 12 12"/></svg>
+                {/if}
+                Route short name: {leg.routeShortName}</span>
         </div>
     </div>
 </div>
