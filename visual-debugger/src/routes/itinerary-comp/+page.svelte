18da1ef47d9ebe48d3c5f49f2ddfc5f2e9633ad5
Kvnstrck <strackekevin@gmail.com>
2025-03-03 17:14:53 +0100

    Feat: Add itinerary comparison page (#66)
    * feat: first functional itinerary comp page
    
    # Conflicts:
    #	visual-debugger/src/sveltestore.ts
    
    * chore: remove unnecessary import
    
    * chore: documentation for persistentStore function
    
    * feat: implement persistent shadow stores
    
    * feat: reset of stores on query upload
    
    ---------
    
    Co-authored-by: MoPhliFra <59418632+MoPhliFra@users.noreply.github.com>
    Co-authored-by: MoPhliFra <mofra0511@gmail.com>

---
+++ visual-debugger/src/routes/itinerary-comp/+page.svelte
@@ -0,0 +1,28 @@
+<script>
+    import DefaultItineraryOverview from "@/components/ui/DefaultItineraryOverview.svelte";
+    import ItineraryOverview from "@/components/ui/ItineraryOverview.svelte";
+    import {currentItineraryStore} from "sveltestore";
+    const i = 0;
+</script>
+
+<div class="h-screen w-screen">
+    <div class="h-1/6 text-center text-2xl content-center">
+        Itinerary Comparison
+    </div>
+    <div class="grid grid-cols-2 h-5/6">
+        <div class="p-2">
+            <div class="p-4 border">
+                <!-- {$currentItineraryStore.duration} -->
+                <DefaultItineraryOverview/>
+            </div>
+            
+        </div>
+        <div class="p-2">
+            
+            <div class="p-4 border">
+                <ItineraryOverview/>
+            </div>
+            
+        </div>
+    </div>
+</div>
