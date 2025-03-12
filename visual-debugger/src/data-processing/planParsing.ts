688ee82776fee0bf54a52c60019ec48300a689b2
Kvnstrck <strackekevin@gmail.com>
2025-03-12 15:01:45 +0100

    fix: test for empty store in computePlan

--- visual-debugger/src/data-processing/planParsing.ts
+++ visual-debugger/src/data-processing/planParsing.ts
@@ -25,7 +25,7 @@ export async function computePlan() {
     })
 
     // if store is empty abort data processing
-    if (queries == undefined) {
+    if (queries == undefined||queries.length == 0) {
         return
     }
 
